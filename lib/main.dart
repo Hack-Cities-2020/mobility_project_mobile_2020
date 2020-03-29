282465fb155c6d0a42573ebe824487918bab5fbf
Marco Leonardini <marco_leonardini@hotmail.com>
2020-03-28 22:00:04 -0400

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'package:mobility_project_mobile_2020/src/ui/home.page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'La Paz Bus GPS Tracker App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
